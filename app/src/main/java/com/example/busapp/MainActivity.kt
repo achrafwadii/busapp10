8f3d2e60aaa93454e57339dab03aeb8fa5266793
dev <younessmessaoudi100@gmail.com>
2025-01-30 23:06:26 +0100

    busapp project > geolocalisation > SplashScreen > restrict the map

--- app/src/main/java/com/example/busapp/MainActivity.kt
+++ app/src/main/java/com/example/busapp/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapFragment
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 
 class MainActivity : AppCompatActivity(),OnMapReadyCallback {
     private var mGoogleMap:GoogleMap?  = null
@@ -25,5 +28,17 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mGoogleMap = googleMap
+        googleMap.uiSettings.isRotateGesturesEnabled = false
+
+        val guelmimBounds = LatLngBounds(
+            LatLng(28.0, -10.5), // Southwest corner
+            LatLng(30.0, -8.5)   // Northeast corner
+        )
+
+        // Restrict the camera to Guelmim-Oued Noun's bounds
+        googleMap.setLatLngBoundsForCameraTarget(guelmimBounds)
+
+        // Move the camera to Guelmim-Oued Noun and set an appropriate zoom level
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(guelmimBounds.center, 8f))
     }
-}
+    }
---
+++ app/src/main/java/com/example/busapp/Main_Menu.java
@@ -0,0 +1,6 @@
+package com.example.busapp;
+
+import android.app.Activity;
+
+public class Main_Menu extends Activity {
+}
